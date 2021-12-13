8d915408f39c14b6971db39ff53ac683d07555d5
bilal2791 <bilalshahzad2791@gmail.com>
2021-12-14 03:52:56 +0500

    update product module

---
+++ resources/views/product.blade.php
@@ -0,0 +1,120 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+
+
+
+<div class="row justify-content-center">
+
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Create Product') }}</div>
+
+                <div class="card-body">
+
+                    <form method="POST" action="{{ route('product.store') }}" enctype="multipart/form-data">
+
+                        @csrf
+
+              <div class="row mb-3">
+                            <label for="title" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+
+                        <div class="row mb-3">
+                            <label for="title" class="col-md-4 col-form-label text-md-right">{{ __('Title') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="title" type="text" class="form-control @error('title') is-invalid @enderror" name="title" value="{{ old('title') }}" required autocomplete="title" autofocus>
+
+                                @error('title')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+
+ <div class="row mb-3">
+                            <label for="image" class="col-md-4 col-form-label text-md-right">{{ __('Image') }}</label>
+
+                            <div class="col-md-6">
+                                <input type="file" name="productImage" id="productImage">
+                            </div>
+                        </div>
+
+
+
+                        <div class="row mb-3">
+                            <label for="price" class="col-md-4 col-form-label text-md-right">{{ __('Price') }}</label>
+
+                            <div class="col-md-6">
+                               <input id="price" type="text" class="form-control @error('price') is-invalid @enderror" name="price" value="{{ old('price') }}" required autocomplete="price" autofocus>
+
+                                @error('price')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+
+        <div class="row mb-3">
+                            <label for="Description" class="col-md-4 col-form-label text-md-right">{{ __('Description') }}</label>
+
+                            <div class="col-md-6">
+                               <textarea class="form-control @error('description') is-invalid @enderror" name="description">{{ old('description') }}</textarea>
+                                @error('description')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+
+
+
+                        <div class="row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Create') }}
+                                </button>
+                            </div>
+                        </div>
+
+
+
+
+        </div>
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+@endsection
