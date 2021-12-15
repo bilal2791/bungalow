36901cc3a7ac24c74c4fbcda8012d435f4fcf770
unknown <salim.info81@gmail.com>
2021-12-15 23:18:39 +0500

    home page done

---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+$('.home_slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: true,
+    prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fas fa-caret-left'></i></button>",
+     nextArrow:"<button type='button' class='slick-next pull-right'><i class='fas fa-caret-left'></i></i></button>"
+});
