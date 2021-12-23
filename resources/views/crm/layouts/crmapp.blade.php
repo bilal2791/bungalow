375014bd4532ac423ef82cf6d9097ac203b9f5b3
unknown <salim.info81@gmail.com>
2021-12-24 02:37:00 +0500

    changes logout dropdown done

--- resources/views/crm/layouts/crmapp.blade.php
+++ resources/views/crm/layouts/crmapp.blade.php
@@ -15,15 +15,12 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-<<<<<<< HEAD
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 
 
     <!-- css of datatable  -->
-=======
   <!-- css of datatable  -->
->>>>>>> cd4aa3770dd62ad8c6ad7365c02d87244a3323da
     <link rel="stylesheet" href="//cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css">
 
     <!-- Styles -->
@@ -34,11 +31,8 @@
 {{-- @include('includes.head'); --}}
 
 
-<<<<<<< HEAD
     <div id="app" class="main-app">
-=======
  <div id="app">
->>>>>>> cd4aa3770dd62ad8c6ad7365c02d87244a3323da
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand mx-auto" href="{{ url('/customer') }}" >
@@ -47,7 +41,6 @@
                 </a>
 
 
-<<<<<<< HEAD
                     <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
                         <div id="navbarSupportedContent" class="collapse navbar-collapse">
                             <ul class="navbar-nav ms-auto">
@@ -76,48 +69,6 @@
                 </button>
             </div>
         </nav>
-=======
-                <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-
-                <div id="page-content-wrapper">
-
-        
-</nav>
->>>>>>> cd4aa3770dd62ad8c6ad7365c02d87244a3323da
-
 
 
         <!-- /#sidebar-wrapper -->
@@ -143,14 +94,6 @@
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-<<<<<<< HEAD
-
-=======
-  	<!-- for js of Datatables  -->
-  	<script src="//cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
-  	<script src="https://cdn.tiny.cloud/1/4t2wjwkuckdyotsl0s30mij4lsjh4e1f3005x1ykbqruct0c/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
-	<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
->>>>>>> cd4aa3770dd62ad8c6ad7365c02d87244a3323da
 
 	  <script>
 
